e568ca9f2fe506018231a1ea9dbf469f5e16823f
DevVad <vadim.rgsu@yandex.ru>
2021-11-17 16:23:46 +0300

    fix margins

--- src/components/personal/personal.tsx
+++ src/components/personal/personal.tsx
@@ -10,19 +10,19 @@ interface PersonalProps {
 }
 
 function Personal(props: PersonalProps) {
-    return (
-        <section className="personal">
-          <h2 className="personal__title">{props.title}</h2>
-          <div className="personal__columns">
-              {props.columnFirst}
-              <img className="personal__chef" src={props.columnFirstImg} alt="Chef man" />
-          </div>
-          <div className="personal__columns">
-              <img className="personal__chef" src={props.columnSecondImg} alt="Chef woman" />
-              {props.columnSecond}
-          </div>
-        </section>
-    );
+  return (
+    <section className="personal">
+      <h2 className="personal__title">{props.title}</h2>
+      <div className="personal__columns">
+        {props.columnFirst}
+        <img className="personal__chef" src={props.columnFirstImg} alt="Chef man" />
+      </div>
+      <div className="personal__columns">
+        <img className="personal__chef" src={props.columnSecondImg} alt="Chef woman" />
+        {props.columnSecond}
+      </div>
+    </section>
+  );
 }
 
 export default Personal;
